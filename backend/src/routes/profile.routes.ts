22c817b8fee68ae941de57d6e236098f8c27e23c
Ravi-Katiyar04 <drxravikatiyar0000@gmail.com>
2026-04-06 00:28:46 +0530

    feat: complete basic  profile page UI

--- backend/src/routes/profile.routes.ts
+++ backend/src/routes/profile.routes.ts
@@ -3,8 +3,6 @@ import { ProfileController } from "../controllers/profile.controller";
 import { authMiddleware } from "../middlewares/auth.middleware";
 
 const router = Router();
-
-// router.post("/", authMiddleware, ProfileController.createProfile);
 router.put("/", authMiddleware, ProfileController.updateProfile);
 
 export default router;
